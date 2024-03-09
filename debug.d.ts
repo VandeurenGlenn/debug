542958273929fe1201cd34b05e58d2e465088462
VandeurenGlenn <vandeurenglenn@gmail.com>
2024-03-09 13:16:12 +0100

    Update debug.d.ts

--- debug.d.ts
+++ debug.d.ts
@@ -1,6 +1,6 @@
 declare global {
   function debug(target: string, text: string): void;
-  var myVariable: number;
+  createDebugger = (target: sting) => debuggerFunction;
 }
 export declare const debuggerFunction = (text: sting) => globalThis.debug;
 export declare const createDebugger = (target: sting) => debuggerFunction;
