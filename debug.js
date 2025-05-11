3ed51a3afbd43de01a3f05d62b4485d6e589da86
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-05-11 15:37:59 +0200

    Update debug.js

--- debug.js
+++ debug.js
@@ -1,10 +1,12 @@
 if (!globalThis.DEBUG) {
   let DEBUG = [];
   if (globalThis.localStorage) {
-    DEBUG = globalThis.localStorage.getItem('DEBUG');
-    globalThis.DEBUG = DEBUG.startsWith('[')
-      ? JSON.parse(DEBUG).split(',')
-      : [DEBUG];
+    DEBUG = globalThis.localStorage.getItem("DEBUG");
+    if (DEBUG) {
+      globalThis.DEBUG = DEBUG.startsWith("[")
+        ? JSON.parse(DEBUG).split(",")
+        : [DEBUG];
+    }
   }
 }
 
@@ -14,14 +16,14 @@ const debug = (target, text, trace) => {
   const _logger = getLogger(trace);
   if (!globalThis.DEBUG || globalThis.DEBUG?.length === 0) return;
   if (
-    globalThis.DEBUG === 'true' ||
+    globalThis.DEBUG === "true" ||
     globalThis.DEBUG === true ||
     globalThis.DEBUG?.indexOf(target) !== -1 ||
-    globalThis.DEBUG?.indexOf('*') !== -1 ||
-    globalThis.DEBUG?.indexOf(target.split('/')[0]) !== -1
+    globalThis.DEBUG?.indexOf("*") !== -1 ||
+    globalThis.DEBUG?.indexOf(target.split("/")[0]) !== -1
   )
-    if (text) _logger('\x1b[34m\x1b[1m%s', `${target}: ${text}`, '\x1b[0m');
-    else _logger('\x1b[34m\x1b[1m%s', `${target}`, '\x1b[0m');
+    if (text) _logger("\x1b[34m\x1b[1m%s", `${target}: ${text}`, "\x1b[0m");
+    else _logger("\x1b[34m\x1b[1m%s", `${target}`, "\x1b[0m");
 };
 
 const createDebugger = (target) => (text) => debug(target, text);
