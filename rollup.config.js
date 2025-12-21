7dc7398cf3ac52c212ccf975e44e7a5c0d0f1739
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-12-21 17:19:18 +0100

    Add rollup-plugin-auto-exports and update Rollup
    Added 'rollup-plugin-auto-exports' to devDependencies and updated Rollup and related platform packages to version 4.54.0. Also updated .gitignore to exclude the 'exports/' directory.

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,28 @@
 import typescript from "@rollup/plugin-typescript";
+import autoExports from "rollup-plugin-auto-exports";
+import { glob, rmdir, unlink } from "fs/promises";
+
+const dir = "exports";
+
+let result = await Array.fromAsync(glob(`${dir}/**/*.*`));
+await Promise.all(
+  result.map(async (file) => {
+    await unlink(file, { recursive: true });
+  })
+);
+
+result = await Array.fromAsync(glob(`${dir}/**/*`));
+await Promise.all(
+  result.map(async (file) => {
+    await rmdir(file);
+  })
+);
 
 export default {
-  input: "debug.ts",
+  input: await Array.fromAsync(glob("src/**/*.ts")),
   output: {
-    file: "exports/debug.js",
+    dir,
     format: "es",
   },
-  plugins: [typescript()],
+  plugins: [typescript(), autoExports()],
 };
